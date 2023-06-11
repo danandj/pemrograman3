18c8ab5840a0660cd304f8b0b28e7f0b4cb6580e
Danandjaya Saputra <danandj@gmail.com>
2023-06-12 06:36:55 +0700

    latihan 1

---
+++ latihan1.2.js
@@ -0,0 +1,16 @@
+//==>how to run javascript syntax
+console.log('hello world');
+
+//==>number in javascript
+console.log(5);
+console.log(10);
+
+//==>calculation in javascript
+console.log(10 + 3);
+
+//==>string in javascript
+console.log('test');
+console.log("test");
+
+//==>combining string in javascript
+console.log("nama " + "saya " + "alfred");
