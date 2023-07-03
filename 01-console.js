3ef5507067dc85018ced7e701a5537002c6617e3
Danandjaya Saputra <danandj@gmail.com>
2023-07-03 20:04:39 +0700

    materi variabel dan console

---
+++ 01-console.js
@@ -0,0 +1,18 @@
+//==>how to run javascript syntax
+console.log('hello world');
+console.error('hello world');
+
+//==>number in javascript
+console.log(5);
+console.log(10);
+
+//==>calculation in javascript
+console.log(10 + 3);
+
+//==>string in javascript
+console.log('test');
+console.log("test");
+
+//==>combining string in javascript
+console.log("nama " + "saya " + "alfred");
+
---
+++ 02-variabel.js
@@ -0,0 +1,40 @@
+//membuat variabel dengan var
+var fullName, height, age;
+fullName = "John Doe";
+height = 170.5;
+age = 40;
+console.log(fullName);
+console.log(height);
+console.log(age);
+
+//membuat variabel dengan let
+let namaLengkap = "John Doe";
+let tinggi = 170.5; 
+let umur = 45;
+console.log(namaLengkap);
+console.log(tinggi);
+console.log(umur);
+
+//membuat variabel dengan const
+const phi = 3.14;
+phi = 100;
+console.log(phi);
+
+//membuat variabel array
+const products = ["computer", "smartphone", "flashdisk"];
+console.log(products);
+console.log(products[1]);
+
+//membuat variabel object
+var car = {
+    type: "AVANZA",
+    model: "2023",
+}
+console.log(car);
+console.log(car.type);
+
+//konversi tipe data dan variabel
+var a = "5" ;
+var b = "7" ;
+var result = parseInt(a) + parseInt(b);
+console.log(result);
